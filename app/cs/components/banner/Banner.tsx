a4a7f3167347edd01fcdc58584c7665fba8c55d7
ondrejchladek <ondrejchladek@gmail.com>
2023-08-21 04:02:43 +0200

    initial

--- app/cs/components/banner/Banner.tsx
+++ app/cs/components/banner/Banner.tsx
@@ -7,10 +7,10 @@ const Banner = () => {
    return (
       <><div className="bg-[url('/bg.png')] bg-no-repeat bg-center bg-cover max-h-[492px] bg-base-100 p-6 sm:p-10 xl:pr-7 xl:pl-16 rounded-xl flex items-center justify-center md:justify-between">
          <div className="w-full md:max-w-[260px] lg:max-w-[440px]">
-            <h1 className="max-w-[400px] text-4xl lg:text-5xl xl:text-[54px] font-semibold text-base-content text-center md:text-start leading-9 xl:!leading-[60px]">
+            <h1 className="max-w-[400px] text-4xl lg:text-5xl xl:text-[54px] font-semibold text-base-content text-center md:text-start leading-9 xl:!leading-[70px]">
                Vize přepsané<br />do precizního kódu
             </h1>
-            <p className="mt-4 mb-6 xl:my-6 text-base xl:text-lg text-base-content/60 text-center md:text-start leading-7 xl:leading-8">
+            <p className="mt-4 mb-6 xl:my-6 text-base xl:text-lg text-base-content/60 text-center md:text-start leading-6 xl:leading-7">
             Jako Full Stack Developer a UI/UX designer se specializuji na tvorbu řešení na front-endu i back-endu se znalostí technologií React, Node.js, Tailwind a TypeScript.
             </p>
             <div className="flex items-center justify-center md:justify-start">
@@ -19,7 +19,7 @@ const Banner = () => {
                   className="btn border-none btn-primary py-1.5 pl-6 pr-1.5 rounded-full flex items-center justify-center gap-5 w-fit"
                >
                   <span className="text-base font-medium text-primary-content">
-                     Napište mi
+                     Kontakt
                   </span>
                   <span className="p-2 bg-primary-content rounded-full">
                      <svg
