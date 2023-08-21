f24894da24351efa072e12d22f0c57a0daa5b9aa
ondrejchladek <ondrejchladek@gmail.com>
2023-08-21 18:07:18 +0200

    initial

--- app/en/components/slider/IconSlider.tsx
+++ app/en/components/slider/IconSlider.tsx
@@ -66,66 +66,66 @@ export default IconSlider
 const IconData = [
    {
       id: 1,
-      icon: <Image src="/1.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/1.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 2,
-      icon: <Image src="/2.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/2.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 3,
-      icon: <Image src="/3.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/3.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 4,
-      icon: <Image src="/4.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/4.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 5,
-      icon: <Image src="/5.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/5.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 6,
-      icon: <Image src="/6.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/6.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 7,
-      icon: <Image src="/7.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/7.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 8,
-      icon: <Image src="/8.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/8.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 9,
-      icon: <Image src="/9.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/9.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 10,
-      icon: <Image src="/10.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/10.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 11,
-      icon: <Image src="/11.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/11.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 12,
-      icon: <Image src="/12.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/12.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 13,
-      icon: <Image src="/13.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/13.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 14,
-      icon: <Image src="/14.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/14.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 15,
-      icon: <Image src="/15.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/15.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
    {
       id: 16,
-      icon: <Image src="/16.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-45" />,
+      icon: <Image src="/16.png" width={100} height={60} alt="Logo" className="w-4/5 opacity-50" />,
    },
 ]
