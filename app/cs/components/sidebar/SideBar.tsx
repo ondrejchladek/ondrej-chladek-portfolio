9bd78588a90265e50a7035daad8c5f2772ae83c0
ondrejchladek <ondrejchladek@gmail.com>
2023-08-29 02:11:25 +0200

    initial

--- app/cs/components/sidebar/SideBar.tsx
+++ app/cs/components/sidebar/SideBar.tsx
@@ -48,6 +48,12 @@ const SideBar = () => {
          {/* info  */}
          <div className="border-b border-base-content/10 py-6 space-y-3">
             <div className="flex items-center justify-between">
+               <p className="text-sm text-base-content">IČ:</p>
+               <span className="text-sm text-base-content/60">
+                  02407604
+               </span>
+            </div>
+            <div className="flex items-center justify-between">
                <p className="text-sm text-base-content">E-mail:</p>
                <a
                   href="mailto: ondrejchladek@gmail.com"
